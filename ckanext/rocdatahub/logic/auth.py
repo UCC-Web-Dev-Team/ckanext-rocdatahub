04ef951bd9d7d6431c7281dd8cebe8e9a3048e46
Clemence Ayekple <clemence.ayekple@ucc.edu.gh>
2023-12-18 10:09:19 +0000

    feat: user registration form updated

--- ckanext/rocdatahub/logic/auth.py
+++ ckanext/rocdatahub/logic/auth.py
@@ -1,4 +1,5 @@
 import ckan.plugins.toolkit as tk
+from ckan import types
 
 
 @tk.auth_allow_anonymous_access
@@ -10,3 +11,12 @@ def get_auth_functions():
     return {
         "rocdatahub_get_sum": rocdatahub_get_sum,
     }
+
+@tk.auth_allow_anonymous_access
+@tk.chained_auth_function
+def user_create(
+    next_func: types.AuthFunction,
+    context: types.Context,
+    data_dict: types.DataDict | None,
+) -> types.AuthResult:
+    return {"success": True}
